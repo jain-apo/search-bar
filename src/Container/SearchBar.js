9923fbbd7002d10ec2dceb95da5b1aeda5f0a2c3
Apoorva Jain <jain.apo@northeastern.edu>
2023-07-12 19:19:28 -0400

    //

--- src/Container/SearchBar.js
+++ src/Container/SearchBar.js
@@ -7,50 +7,7 @@ const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-  // const handleSearch = () => {
-  //   setLoading(true);
-  //   setResults([]);
-  
-  //   const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
-  //   const apiUrl = `https://testtechnext1-pearl118.b4a.run/search/api/query/?query=${searchTerm}`;
-  //   const url = corsAnywhereUrl + apiUrl;
-    
-  //   console.log(url);
-    
-  //   const xhr = new XMLHttpRequest();
-    
-  //   xhr.open('GET', url, true);
-  //   xhr.setRequestHeader('Accept', 'application/json, text/plain, */*');
-  //   xhr.setRequestHeader('Content-Type', 'application/json');
-  //   xhr.setRequestHeader('origin', 'https://fleek.ipfs.io'); // Add the origin header
-    
-  //   xhr.onreadystatechange = function () {
-  //     if (xhr.readyState === XMLHttpRequest.DONE) {
-  //       if (xhr.status === 200) {
-  //         try {
-  //           console.log(xhr.responseText);
-  //           const response = JSON.parse(xhr.responseText);
-  //           setResults(response);
-  //         } catch (error) {
-  //           console.log('Error parsing response:', error);
-  //         }
-  //       } else {
-  //         console.log('Request failed with status:', xhr.status);
-  //       }
-    
-  //       setLoading(false);
-  //       if (xhr.responseText.includes('No Access-Control-Allow-Origin')) {
-  //         alert('CORS error: No Access-Control-Allow-Origin header');
-  //       } else if (JSON.parse(xhr.responseText).length === 0) {
-  //         alert('No Records Found');
-  //       }
-  //     }
-  //   };
-    
-  //   xhr.send();
-    
-  // };
-  
+
   const handleSearch = async () => {
     setLoading(true);
     setResults([]);
