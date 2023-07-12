403a879e02f759556040f87bbebe0c02d2861f8e
Apoorva Jain <jain.apo@northeastern.edu>
2023-07-12 19:14:49 -0400

    ..

--- src/Container/SearchBar.js
+++ src/Container/SearchBar.js
@@ -7,34 +7,77 @@ const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  const handleSearch = () => {
+  // const handleSearch = () => {
+  //   setLoading(true);
+  //   setResults([]);
+  
+  //   const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
+  //   const apiUrl = `https://testtechnext1-pearl118.b4a.run/search/api/query/?query=${searchTerm}`;
+  //   const url = corsAnywhereUrl + apiUrl;
+    
+  //   console.log(url);
+    
+  //   const xhr = new XMLHttpRequest();
+    
+  //   xhr.open('GET', url, true);
+  //   xhr.setRequestHeader('Accept', 'application/json, text/plain, */*');
+  //   xhr.setRequestHeader('Content-Type', 'application/json');
+  //   xhr.setRequestHeader('origin', 'https://fleek.ipfs.io'); // Add the origin header
+    
+  //   xhr.onreadystatechange = function () {
+  //     if (xhr.readyState === XMLHttpRequest.DONE) {
+  //       if (xhr.status === 200) {
+  //         try {
+  //           console.log(xhr.responseText);
+  //           const response = JSON.parse(xhr.responseText);
+  //           setResults(response);
+  //         } catch (error) {
+  //           console.log('Error parsing response:', error);
+  //         }
+  //       } else {
+  //         console.log('Request failed with status:', xhr.status);
+  //       }
+    
+  //       setLoading(false);
+  //       if (xhr.responseText.includes('No Access-Control-Allow-Origin')) {
+  //         alert('CORS error: No Access-Control-Allow-Origin header');
+  //       } else if (JSON.parse(xhr.responseText).length === 0) {
+  //         alert('No Records Found');
+  //       }
+  //     }
+  //   };
+    
+  //   xhr.send();
+    
+  // };
+  
+  const handleSearch = async () => {
     setLoading(true);
     setResults([]);
-    const xhr = new XMLHttpRequest();
-    const url = `https://testtechnext1-pearl118.b4a.run/search/api/query/?query=${searchTerm}`;
-
-    xhr.open("GET", url, true);
-    xhr.setRequestHeader("Accept", "application/json, text/plain, */*");
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onreadystatechange = function () {
-
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          setResults(JSON.parse(xhr.responseText));
-        } else {
-          console.log("Error");
-        }
-        setLoading(false);
-        if(JSON.parse(xhr.responseText).length==0)
-          alert("No Records Found")
+  
+    const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = `https://testtechnext1-pearl118.b4a.run/search/api/query/?query=${searchTerm}`;
+    const url = corsAnywhereUrl + apiUrl;
+  
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        setResults(data);
+        if (data.length === 0) {
+          alert('No Records Found');
+    }
+      } else {
+        console.log('Request failed with status:', response.status);
       }
-    };
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  
+    setLoading(false);
     
-    xhr.send();
-
   };
+  
 
 
   return (
